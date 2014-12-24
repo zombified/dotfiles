2bb3ee1cf1d8be43d424e217f13f3f800177fd01
Joel Kleier <joel@kleier.us>
2014-12-24 11:30:46 -0600

    added commands for moving window to screens to the left/right

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -88,3 +88,63 @@ hs.hotkey.bind({"cmd", "alt"}, "Down", function()
     win:setFrame(f, 0)
 end)
 
+-- Move to next screen on right/to the east
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+    -- no point in moving a window if there are no screens to move it too
+    local allscreens = hs.screen.allScreens()
+    local cnt = 0
+    -- don't know why, but table.getn is a "nil value"
+    for k, v in pairs(allscreens) do
+        cnt = cnt + 1
+    end
+    if cnt < 1 then
+        return
+    end
+
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local sf = screen:frame()
+
+    local east_screen = screen:toEast()
+    -- no point in moving the window if there is no easterly screen
+    if east_screen == nil then
+        return
+    end
+    local esf = east_screen:frame()
+
+    f.x = esf.x
+
+    win:setFrame(f, 0)
+end)
+
+-- Move to next screen on left
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+    -- no point in moving a window if there are no screens to move it too
+    local allscreens = hs.screen.allScreens()
+    local cnt = 0
+    -- don't know why, but table.getn is a "nil value"
+    for k, v in pairs(allscreens) do
+        cnt = cnt + 1
+    end
+    if cnt < 1 then
+        return
+    end
+
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local sf = screen:frame()
+
+    local west_screen = screen:toWest()
+    -- no point in moving the window if there is no easterly screen
+    if west_screen == nil then
+        return
+    end
+    local wsf = west_screen:frame()
+
+    f.x = wsf.x
+
+    win:setFrame(f, 0)
+
+end)
